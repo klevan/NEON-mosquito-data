b0f81d3f75d4ab12e6b8f4432ff0709dac0b321d
Katie LeVan <katherine.levan@gmail.com>
2016-06-23 18:51:39 -0600

    update species composition

--- code/summary stats.R
+++ code/summary stats.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 library(dplyr)
+library(graphics)
 options(stringsAsFactors = F)
 
 # NOTE: Run 'getting-the-data.R' first before running this script
@@ -41,6 +42,28 @@ class(sampling) <- "data.frame"
 sampling$startCollectDate <- as.Date(sampling$startCollectDate)
 sampling$adjustedAbundance <- sampling$estimatedAbundance/sampling$totalTrapHours
 
+# Species table
+# EventID is unique to each bout of sampling
+sppList <- sort(unique(id$scientificName[grepl("species", id$taxonRank)]))
+spp <- unique.data.frame(sampling[, c("siteID", "eventID", "startCollectDate")])
+spp <- cbind(spp, matrix(data = rep(0, length(sppList)*dim(spp)[1]), 
+                             nrow = dim(spp)[1], ncol = length(sppList), 
+                             dimnames = list(c(1:dim(spp)[1]), sppList)))
+for (i in spp$eventID){
+  rows <- which(id$eventID==i)
+  spp$estimatedAbundance[spp$eventID==i] <- sum(id$estimatedAbundance[rows])
+  spp$richness[spp$eventID==i] <- length(unique(id$scientificName[id$eventID==i & 
+                                                                  grepl("species", 
+                                                                        id$taxonRank)]))
+  for (j in which(colnames(spp)%in%unique(id$scientificName))){
+    spp[which(spp$eventID==i), j] <- round(sum(id$estimatedAbundance[id$eventID==i & 
+                                                               grepl(colnames(spp)[j], 
+                                                                     id$scientificName)])/
+                                             spp$estimatedAbundance[spp$eventID==i], 2)
+  }
+}
+
+
 # Pathogen status
 pathogenStatus <- summarize(group_by(path[path$testNumber==1, ], testingID),
                             siteID = unique(siteID),
@@ -71,17 +94,17 @@ pathogenStatus$minPropPositive <- ((pathogenStatus$minPositiveMosquitoes/
                                      pathogenStatus$numMosquitoesTested)*
                                      pathogenStatus$relativeAbundance)
 
-# Relationship between Abundance & Richness
-par(mfrow = c(1, 1))
-plot(x = sampling$estimatedAbundance,
-     y = sampling$richness,
-     xlab = "Estimated mosquito abundance",
-     ylab = "Richness", pch = 21, bg = "forestgreen",
-     cex = 1.5)
+sampling <- left_join(sampling, 
+                      pathogenStatus[!is.na(pathogenStatus$maxPropPositive), 
+                                     c(3, 11, 12)],
+                      by = "eventID")
+
+sampling[is.na(sampling$maxPropPositive), 10:11] <- 0
 
 # Plotting Abundance and Richness
+par(mfrow = c(2, 2))
 for (i in unique(sampling$siteID)[c(3, 1, 2, 4)]){
-  par(mfrow = c(2, 2), mar = c(5, 5, 2, 5))
+  par(mar = c(5, 5, 2, 5))
   with(sampling[sampling$siteID==i, ], 
        plot(startCollectDate, estimatedAbundance, 
             type="l", col="darksalmon",
@@ -106,10 +129,141 @@ for (i in unique(sampling$siteID)[c(3, 1, 2, 4)]){
   }
 }
 
-# Pathogen status within the mosquito community
+# Species Composition
+tenMosquitoes <- sort(sapply(spp[, 4:45], max), 
+                      decreasing = T)[1:10]
+
+for (i in unique(spp$siteID)[c(3,1,2,4)]){
+  par(mar = c(5, 5, 2, 5))
+  plot(spp[spp$siteID==i, "startCollectDate"], 
+       spp[spp$siteID==i, 4], 
+       type="n", ylim = c(0, 1), ylab="Relative abundance", 
+       main = i, xlab = "Date")
+  t = 1
+  for (j in sort(names(tenMosquitoes))){
+    par(new = T)
+    col <- colSums(spp[, 4:45])
+    plot(spp[spp$siteID==i, "startCollectDate"], 
+         spp[spp$siteID==i, j], 
+         type="l", col = rainbow(10)[t], 
+         ylim = c(0, 1),
+         xlim = c(min(sampling$startCollectDate),
+                  max(sampling$startCollectDate)),
+         ylab = NA, main = i, lwd = 3,
+         xlab = NA, axes = F)
+    t = t + 1
+  }
+  if (i == "STER"){
+    legend(x = "topleft", legend = sort(names(tenMosquitoes)),
+           col = rainbow(10), pch = rep(NA, 10),
+           lwd = 2, lty = 1)
+  }
+}
+
+
+
+# Relationship between Abundance & Richness
 par(mfrow = c(1, 1))
 plot(x = sampling$estimatedAbundance,
      y = sampling$richness,
      xlab = "Estimated mosquito abundance",
      ylab = "Richness", pch = 21, bg = "forestgreen",
      cex = 1.5)
+
+# Pathogen status within the mosquito community
+# Central Plains Experimental Range 
+with(sampling[sampling$siteID=="CPER", ], 
+     plot(x = startCollectDate,
+          y = estimatedAbundance,
+          xlab = "Date", main = "Central Plains Experimental Range (CPER)",
+          ylab = "Estimated mosquito abundance", 
+          type = "l", lwd = 2,
+          col = "black"))
+par(new = T)
+with(sampling[sampling$siteID=="CPER", ], 
+     plot(startCollectDate, maxPropPositive,
+          pch = 21, axes = F, xlab = NA, cex = 1.5,
+          ylim = c(0, .1),
+          ylab = NA, cex.lab = 1, bg = "darksalmon"))
+par(new = T)
+with(sampling[sampling$siteID=="CPER", ], 
+     plot(startCollectDate, minPropPositive,
+          pch = 21, axes = F, xlab = NA, cex = 1.5,
+          ylim = c(0, .1),
+          ylab = NA, cex.lab = 1, bg = "forestgreen"))
+abline(h = .001, lty = 2)
+axis(side = 4)
+mtext(side = 4, line = 3, 'Proportion Positive for West Nile')
+legend("topleft",
+       legend = c("Abundance (all mosquitoes)",  
+                  "Expected infection rate",
+                  "Measured max infection rate",
+                  "Measured min infection rate"),
+       lty = c(1, 2, 0, 0), pch=c(NA, NA, 16, 16), lwd = c(1.5, 1, 0, 0), 
+       col = c("black", "black", "darksalmon", "forestgreen"))
+box()
+
+# North Sterling, CO 
+with(sampling[sampling$siteID=="STER", ], 
+     plot(x = startCollectDate,
+          y = estimatedAbundance,
+          xlab = "Date", main = "North Sterling, CO (STER)",
+          ylab = "Estimated mosquito abundance", 
+          type = "l", lwd = 2,
+          col = "black"))
+par(new = T)
+with(sampling[sampling$siteID=="STER", ], 
+     plot(startCollectDate, maxPropPositive,
+          pch = 21, axes = F, xlab = NA, cex = 1.5,
+          ylim = c(0, .5),
+          ylab = NA, cex.lab = 1, bg = "darksalmon"))
+par(new = T)
+with(sampling[sampling$siteID=="STER", ], 
+     plot(startCollectDate, minPropPositive,
+          pch = 21, axes = F, xlab = NA, cex = 1.5,
+          ylim = c(0, .5),
+          ylab = NA, cex.lab = 1, bg = "forestgreen"))
+abline(h = .001, lty = 2)
+axis(side = 4)
+mtext(side = 4, line = 3, 'Proportion Positive for West Nile')
+legend("topleft",
+       legend = c("Abundance (all mosquitoes)",  
+                  "Expected infection rate",
+                  "Measured max infection rate",
+                  "Measured min infection rate"),
+       lty = c(1, 2, 0, 0), pch=c(NA, NA, 16, 16), lwd = c(1.5, 1, 0, 0), 
+       col = c("black", "black", "darksalmon", "forestgreen"))
+box()
+
+
+# Ordway-Swisher Biological Station
+with(sampling[sampling$siteID=="OSBS", ], 
+     plot(x = startCollectDate,
+          y = estimatedAbundance,
+          xlab = "Date", main = "Ordway-Swisher Biological Station (OSBS)",
+          ylab = "Estimated mosquito abundance", 
+          type = "l", lwd = 2,
+          col = "black"))
+par(new = T)
+with(sampling[sampling$siteID=="OSBS", ], 
+     plot(startCollectDate, maxPropPositive,
+          pch = 21, axes = F, xlab = NA, cex = 1.5,
+          ylim = c(0, .5),
+          ylab = NA, cex.lab = 1, bg = "darksalmon"))
+par(new = T)
+with(sampling[sampling$siteID=="OSBS", ], 
+     plot(startCollectDate, minPropPositive,
+          pch = 21, axes = F, xlab = NA, cex = 1.5,
+          ylim = c(0, .5),
+          ylab = NA, cex.lab = 1, bg = "forestgreen"))
+abline(h = .003, lty = 2)
+axis(side = 4)
+mtext(side = 4, line = 3, 'Proportion Positive for Eastern Equine')
+legend("topleft",
+       legend = c("Abundance (all mosquitoes)",  
+                  "Expected infection rate",
+                  "Measured max infection rate",
+                  "Measured min infection rate"),
+       lty = c(1, 2, 0, 0), pch=c(NA, NA, 16, 16), lwd = c(1.5, 1, 0, 0), 
+       col = c("black", "black", "darksalmon", "forestgreen"))
+box()
